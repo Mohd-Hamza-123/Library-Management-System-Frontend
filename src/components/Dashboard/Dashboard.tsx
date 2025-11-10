2e653187514b049fb37aab7c31047197ece1388d
Mohd Hamza <mohdhamza.me@gmail.com>
2025-11-10 23:31:20 +0530

    ui : add bg image in dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import '@/styles/dashboard.css'
 import { getDateTime } from '@/utils/dates'
 
 export default function Dashboard() {
-   
+
     return (
-        <main className='bg-gray-100/90 w-full rounded-tl-xl px-5 py-5'>
-            <h3 className='text-xl text-gray-800 font-bold'>Welcome to Dashboard</h3>
-            <span className='text-sm text-gray-600'>{getDateTime()}</span>
+        <main className='bg-gray-100/90 w-full rounded-tl-xl overflow-x-hidden'>
+            <section className='dashboard-bg-image px-5 py-5'>
+                <h3 className='text-2xl text-gray-300 font-md'>Welcome to Dashboard</h3>
+                <span className='text-sm text-gray-200'>{getDateTime()}</span>
+            </section>
+
         </main>
     )
 }
