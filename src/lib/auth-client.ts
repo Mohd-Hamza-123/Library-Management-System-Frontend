8f07d35328c3bc74d73724ee6143992b4cb4913d
Mohd Hamza <mohdhamza.me@gmail.com>
2025-12-11 21:55:26 +0530

    bug : fix base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import conf from "@/conf/conf"
 import { createAuthClient } from "better-auth/react"
-console.log(conf.BETTER_AUTH_URL)
+
 export const authClient = createAuthClient({
-    baseURL: process.env.NODE_ENV === "production" ? conf.BASE_URL : "http://localhost:3001/"
+    baseURL: process.env.NODE_ENV === "production" ? conf.BETTER_AUTH_URL : "http://localhost:3000/"
 })
 
